1a4b25d023fe333acd9e35ff5d1574303f7a93da
Akshit Sethi <akshitsethi@gmail.com>
2017-12-28 16:15:36 +0530

    Subscription form added.

--- app/views/footer.php
+++ app/views/footer.php
@@ -15,6 +15,20 @@ if ( ! defined( 'APP' ) ) {
 
 </div><!-- #page -->
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5a269169d0795768aaf8d736/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+<script type="text/javascript" src="//downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us17.list-manage.com","uuid":"c3c3ce7661927a2cae7578546","lid":"cfdaf904aa"}) })</script>
 <!--- Good, Better, Best. Never let it rest. Till your good is better and your better is best. -->
 </body>
 </html>
