5be6b17b9c3e91ce062e87976a17eb0e75c5ec2f
Akshit Sethi <akshitsethi@gmail.com>
2019-01-08 02:42:36 +0530

    Added meta tags.

--- app/views/header.php
+++ app/views/header.php
@@ -12,6 +12,8 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php echo $meta['title']; ?></title>
+	<meta name="description" content="Developer by profession & Entrepreneur by passion. I create stuff for the web.">
+	<meta name="keywords" content="akshitsethi, akshit sethi, akshit, developer, designer, entrepreneur, web applications, wordpress developer, wordpress themes, wordpress plugins, wp news, php, mysql, html5, css, js, javascript developer, es6, jquery, sass, gulp, reactjs, git, react-native, redux, nodejs, lamp, mobile apps">
 	<link rel="shortcut icon" href="app/img/logo.png">
 	<link href="https://fonts.googleapis.com/css?family=VT323" rel="stylesheet" />
 	<link href="app/css/app.css" rel="stylesheet" id="app-css" type="text/css" media="all" />
