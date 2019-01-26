5fcc29529118eb5b162dc91270b2262227754ce4
Akshit Sethi <akshitsethi@gmail.com>
2019-01-27 02:47:35 +0530

    Google analytics + tawk.to scripts.

--- app/views/header.php
+++ app/views/header.php
@@ -8,6 +8,13 @@
 
 ?><!DOCTYPE html>
 <head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11594809-18"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-11594809-18');
+	</script>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
