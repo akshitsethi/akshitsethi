0f0eea46b4213e62b75cfa6d835f3a0141effc50
Akshit Sethi <akshitsethi@gmail.com>
2019-07-13 03:33:45 +0530

    Save changes till now.

--- waste/test.php
+++ waste/test.php
@@ -1,33 +1,7 @@
 <?php
-/**
- * Class for handling all the learning stuff.
- *
- * @package akshitsethi
- */
 
-class AkshitSethi {
+$url = 'something';
 
-	public function __construct() {
-		// Let's do some interesting stuff here and then we move to other things around it.
-		$this->first_lesson();
-	}
+$content = 'Replace me %s %s here';
 
-	/**
-	 * Let's call our first function and let it do something interesting which is unique as well as new for me. It is all for the sake of learning.
-	 *
-	 * @access public
-	 */
-	public function first_lesson() {
-		
-	}
-
-	/**
-	 * This is going to be our second function and we are going to continue spreading the awesomeness which we started with the first one. Makes sense?
-	 *
-	 * @access public
-	 */
-	public function second_lesson() {
-		
-	}
-
-}
+echo sprintf( $content, $url, $url );
